59d7db18f4ec062f0aa720757f8585508d72bcb3
kennyhakazimana10 <kennyhakazimana10@gmail.com>
2025-11-02 14:38:45 -0500

    Add initial setup for Soccer Giphy Fetch

---
+++ script.js
@@ -0,0 +1,10 @@
+// Soccer Giphy Fetch Assignment
+// [Kenny-All Hakazimana]
+
+console.log("script.js connected");
+
+// API setup
+const apiKey = "YOUR_API_KEY_HERE";
+const button = document.querySelector("#fetch-btn");
+const container = document.querySelector("#gif-container");
+const searchInput = document.querySelector("#search-input");
